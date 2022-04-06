73492655100f892a9fcadaa0dfa3376b83ca0683
Alexandros Kyprianidis <alexkypr.ece@gmail.com>
2022-04-06 23:28:25 +0300

    Fix manufacturer column name

--- airplane_api/airplane/models.py
+++ airplane_api/airplane/models.py
@@ -52,17 +52,17 @@ class ManufacturerModel(db.Model):
     that will have Boeing as manufacturer and a320 as model.
     """
     __tablename__ = 'manufacturer_model'
-    make = db.Column(db.String(20), db.ForeignKey('manufacturer.name'), primary_key=True)
+    manufacturer = db.Column(db.String(20), db.ForeignKey('manufacturer.name'), primary_key=True)
     model = db.Column(db.String(20), db.ForeignKey('airplane_model.name'), primary_key=True)
 
 @event.listens_for(ManufacturerModel.__table__, 'after_create')
 def populate_manufacturer_model(*args, **kwargs):
-    db.session.add(ManufacturerModel(make='Boeing', model='747-8'))
-    db.session.add(ManufacturerModel(make='Boeing', model='767'))
-    db.session.add(ManufacturerModel(make='Boeing', model='777'))
-    db.session.add(ManufacturerModel(make='Airbus', model='A220'))
-    db.session.add(ManufacturerModel(make='Airbus', model='A330'))
-    db.session.add(ManufacturerModel(make='Airbus', model='A350'))
+    db.session.add(ManufacturerModel(manufacturer='Boeing', model='747-8'))
+    db.session.add(ManufacturerModel(manufacturer='Boeing', model='767'))
+    db.session.add(ManufacturerModel(manufacturer='Boeing', model='777'))
+    db.session.add(ManufacturerModel(manufacturer='Airbus', model='A220'))
+    db.session.add(ManufacturerModel(manufacturer='Airbus', model='A330'))
+    db.session.add(ManufacturerModel(manufacturer='Airbus', model='A350'))
     db.session.commit()
 
 class Airplane(db.Model):
@@ -80,7 +80,7 @@ class Airplane(db.Model):
     fuel_capacity = db.Column(db.Integer, nullable=True)
     next_destination = db.Column(db.String(20), nullable=True)
     __table_args__ = (db.ForeignKeyConstraint([manufacturer, model],
-                                           [ManufacturerModel.make, ManufacturerModel.model]), {}
+                                           [ManufacturerModel.manufacturer, ManufacturerModel.model]), {}
                       )
 
     def as_dict(self):
